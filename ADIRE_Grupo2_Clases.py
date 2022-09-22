5216ee6b7b92f4e82f2f3bf9b3c4c8bcb6aff03c
OwenGaretto <owen.74011@gmail.com>
2022-09-22 08:21:49 -0300

    ADIRE_grupo_2

--- ADIRE_Grupo2_Clases.py
+++ ADIRE_Grupo2_Clases.py
@@ -8,7 +8,7 @@ class personal:
         self.nombre = nombre
         self.apellido = apellido
         self.dni = dni 
-        self.fnacimiento = fnacimiento
+        self.fnacimiento = fnacimiento #muy buen trabajo
         self.ID = ID 
         self.numtelefono = numtelefono
         self.gmail = gmail
@@ -54,7 +54,6 @@ Luciana = personal("Cadete", "Santa Rosa", "SI","Luciana Rodriguez", "luciana450
 Jorge = personal("Cadete", "VGB", "SI","Jorge Manolo", "bomberos VGB","Jorge","Manolo","42.162.396","14/9/1999", "9","+54 3546 89 2636","jorgemano@gmail.com", "ojo de agua 1")
 Elizabeth = personal("Cabo", "VGB", "SI","Eliz Paredes", "bomberos VGB","Elizabeth","Paredes","36.364.291","20/12/2000", "10","+54 3546 10 9306","Elizabethsi@gmail.com", "Magnolias 23")
 
-
 class incidente:
     def __init__(self, tipo, lugar, damnificados, horasalida, fechasalida, horaretorno, fecharetorno, moviles, danos, horaincidente, horallegada):
         self.tipo = tipo
@@ -93,3 +92,44 @@ class registros_economicos:
         self.motivoingreso = motivoingreso
         self.motivoegreso = motivoegreso
         self.nfactura = nfactura
+
+dotacion = [personal, rodados, incidente]
+print("Que archivos quieres ver? Opcion 1: Persona, Opcion 2: rodados")
+opcion = float(input())
+if opcion==1:
+    print("Bombero 1:", julio.cargo, julio.cuartel, julio.servicio, julio.nombreusuario, julio.contrasena, julio.nombre, julio.apellido, julio.dni, julio.fnacimiento, julio.ID, julio.numtelefono, julio.gmail, julio.domicilio)
+    print("Bombero 2:", pedro.cargo, pedro.cuartel, pedro.servicio, pedro.nombreusuario, pedro.contrasena, pedro.nombre, pedro.apellido, pedro.dni, pedro.fnacimiento, pedro.ID, pedro.numtelefono, pedro.gmail, pedro.domicilio)
+    print("Bombero 3:", pamela.cargo, pamela.cuartel, pamela.servicio, pamela.nombreusuario, pamela.contrasena, pamela.nombre, pamela.apellido, pamela.dni, pamela.fnacimiento, pamela.ID, pamela.numtelefono, pamela.gmail, pamela.domicilio)
+    print("Bombero 4:", martina.cargo, martina.cuartel, martina.servicio, martina.nombreusuario, martina.contrasena, martina.nombre, martina.apellido, martina.dni, martina.fnacimiento, martina.ID, martina.numtelefono, martina.gmail, martina.domicilio)
+    print("Bombero 5:", Thomas.cargo, Thomas.cuartel, Thomas.servicio, Thomas.nombreusuario, Thomas.contrasena, Thomas.nombre, Thomas.apellido, Thomas.dni, Thomas.fnacimiento, Thomas.ID, Thomas.numtelefono, Thomas.gmail, Thomas.domicilio)
+    print("Bombero 6:", Owen.cargo, Owen.cuartel, Owen.servicio, Owen.nombreusuario, Owen.contrasena, Owen.nombre, Owen.apellido, Owen.dni, Owen.fnacimiento, Owen.ID, Owen.numtelefono, Owen.gmail, Owen.domicilio)
+    print("Bombero 7:", Matias.cargo, Matias.cuartel, Matias.servicio, Matias.nombreusuario, Matias.contrasena, Matias.nombre, Matias.apellido, Matias.dni, Matias.fnacimiento, Matias.ID, Matias.numtelefono, Matias.gmail, Matias.domicilio)
+    print("Bombero 8:", Luciana.cargo, Luciana.cuartel, Luciana.servicio, Luciana.nombreusuario, Luciana.contrasena, Luciana.nombre, Luciana.apellido, Luciana.dni, Luciana.fnacimiento, Luciana.ID, Luciana.numtelefono, Luciana.gmail, Luciana.domicilio)
+    print("Bombero 9:", Jorge.cargo, Jorge.cuartel, Jorge.servicio, Jorge.nombreusuario, Jorge.contrasena, Jorge.nombre, Jorge.apellido, Jorge.dni, Jorge.fnacimiento, Jorge.ID, Jorge.numtelefono, Jorge.gmail, Jorge.domicilio)
+    print("Bombero 10:", Elizabeth.cargo, Elizabeth.cuartel, Elizabeth.servicio, Elizabeth.nombreusuario, Elizabeth.contrasena, Elizabeth.nombre, Elizabeth.apellido, Elizabeth.dni, Elizabeth.fnacimiento, Elizabeth.ID, Elizabeth.numtelefono, Elizabeth.gmail, Elizabeth.domicilio)
+
+else:
+    print("vehiculo 1", nombre1.patente, nombre1.fechavencimiento, nombre1.numeropoliza, nombre1.seguro)
+    print("vehiculo 2", nombre2.patente, nombre2.fechavencimiento, nombre2.numeropoliza, nombre2.seguro)
+    print("vehiculo 3", nombre3.patente, nombre3.fechavencimiento, nombre3.numeropoliza, nombre3.seguro)
+
+print("¿Desea ingresar una dotacion?")
+a= input()
+if a == "si" or "Si" or "SI":
+    print("Ingrese los datos de dotacion")
+    d1 = input("Nombre/identificacion de la dotacion")
+    d2 = input("Nombre de bomberos: ")
+    d3 = input("Apellido: ")
+    d4 = input("Cargos: ")
+    d4_5 = input("Motico")
+    d5 = input("Movil: ")
+    d5_5 = input("Ubicacion")
+    d6 = input("Fecha de salida: ")
+    d7 = input("Fecha de llegada: ")
+    d8 = input("Hora de salida: ")
+    d9 = input("Hora de llegada: ")
+    
+    print ("La dotacion", d1, "salió a las", d8, "del dia", d6, "a", d5_5, "por un ", d4_5, "llegando a la ubicacion a las", d9, "del", d7)
+
+elif a == "no" or "No" or "NO": 
+    print("OK.")
