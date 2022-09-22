aa58a80f4724960515bac6195a9283f023f87614
lucila567 <>
2022-09-22 09:13:14 -0300

    Update ADIRE_grupo1_clases.py

--- ADIRE_grupo1_clases.py
+++ ADIRE_grupo1_clases.py
@@ -1,5 +1,5 @@
 class personas:
-    def __init__(self, nombre,apellido, direccion, fecha_de_nacimiento, DNI, numero_de_telefono, ID, gmail):
+    def __init__(self, nombre,apellido, direccion, fecha_de_nacimiento, DNI, numero_de_telefono, ID, gmail,rango_cargo, cuartel, usuario, contraseña):
         self.nombre = nombre
         self.apellido =apellido
         self.direccion=direccion
@@ -8,10 +8,6 @@ class personas:
         self.numero_de_telefono=numero_de_telefono
         self.ID=ID
         self.gmail=gmail
-    
-
-class bombero:
-    def __init__(self, rango_cargo, cuartel, usuario, contraseña):
         self.rango_cargo=rango_cargo
         self.cuartel=cuartel
         self.usuario=usuario
@@ -42,6 +38,22 @@ class bombero:
         print("ver registro economico")
     def modificar_datos_personales(self):
         print("modificar datos personales")
+persona1=("carla","lopez","los manantiales","2_10_2006","46716890","354678659","11.11.12","lopezcarla@gmail.com","cadete","la cumbrecita","carlafernandez","tu casita sabrosa")
+print("Ingrese una persona")
+a = input("Nombre: ")
+b = input("Apellido: ")
+c = input("direccion")
+d = input("Fecha de nacimento: ")
+e = input("DNI: ")
+f = input("Numero de telefono: ")
+g = input("ID: ")
+h = input ("gmail: ")
+i = input("Cargo: ")
+j = input("Cuartel: ")
+k = input("Nombre de usuario: ")
+l = input("Contraseña: ")
+persona2 = (a,b,c,d,e,f,g,h,i,j,k,l)
+print(persona2)
 class incidente:
     def __init__(self, ubicacion_del_incidente, tipo, damnificado, dotacion, fecha_de_salida, hora_de_salida, fecha_de_retorno, hora_de_retorno, datos_del_informante, moviles, daños, hora_del_incidente, hora_de_llegada, hora_de_llegada_de_la_dotacion_al_lugar_del_incidente):
         self.ubicacion_del_incidente=ubicacion_del_incidente
@@ -62,7 +74,7 @@ class rodado:
         self.fecha_de_nacimiento=fecha_de_nacimiento
         self.tipo=tipo
         self.estado=estado
-
+rodado1=("abc123", "sos", "14583965", "2010", "autobomba", "nueva")
 class registros_economicos:
     def __init__(self,ingresos,egresos,fecha,motivo_de_ingreso,motivo_de_egreso,numero_de_factura):
         self.ingresos=ingresos
