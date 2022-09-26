6d195e4f8fb42889a53555192b4e9fdb8634bb66
FNCasd <112396652+FNCasd@users.noreply.github.com>
2022-09-26 09:19:54 -0300

    26 de septiembre
    verificacion de el ingreso de bomberos en una dotacion e impresion de todos los ingresados

--- ADIRE_Grupo4_Clases.py
+++ ADIRE_Grupo4_Clases.py
@@ -2,7 +2,7 @@
 from kivy.lang import Builder
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen"""
-class persona:
+class personal:
     def __init__ (self, nombre, apellido, dni, fnacimiento, ID, numtelefono, gmail, domicilio, rol):
         self.nombre = nombre
         self.apellido = apellido
@@ -13,8 +13,12 @@ class persona:
         self.gmail = gmail
         self.domicilio = domicilio
         self.rol = rol
+    def imprimir(self):
+        print(self.nombre, self.apellido, self.dni, self.fnacimiento, self.ID, self.numtelefono, self.gmail, self.domicilio, self.rol)
+        print("")
+        
 
-class bomberos(persona):
+class bomberos(personal):
     def __init__(self, cargo, cuartel, servicio, nombreusuario, contrasenia, iniciarsesion, cargardatos, editardatos, verdatos, cargararticulos, verarticulos, modificararticulos, cargarrodados, modificarrodados, verrodados, cargarregistroeconomico, modificarregistroeconomico, verregistroeconomico, editarcuenta):
         self.cargo = cargo
         self.cuartel = cuartel
@@ -22,10 +26,9 @@ class bomberos(persona):
         self.nombreusuario = nombreusuario
         self.contrasenia = contrasenia
     def iniciarsesion(self):
-        tr= input("ingrese nombre de usuario: ")
-        rt= input("ingrese su contrasenia: ")
+        pass
     def cargardatos(self):
-        
+        pass
     def editardatos(self):
         pass
     def verdatos(self):
@@ -49,7 +52,10 @@ class bomberos(persona):
     def verregistroeconomico(self):
         pass 
     def editarcuenta(self):
-        pass 
+        pass
+    def imprimir(self):
+        print(self.nombre, self.apellido, self.dni, self.fnacimiento, self.numtelefono, self.gmail, self.domicilio, self.rango)    
+        print("")
         
 class incidente:
     def __init__(self, tipo, lugar, damnificados, horadesalida, fechadesalida, horaderetorno, fechaderetorno, moviles, daños, horadelincidente, horadellegada):
@@ -73,11 +79,15 @@ class articulos:
         self.cantidad = cantidad
 
 class rodados:
-    def __init__(self, patente, seguro, numerodepoliza, fechadevencimiento):
+    def __init__(self, patente, seguro, numerodepoliza, fechadevencimiento, equipamiento):
         self.patente = patente
         self.seguro = seguro
         self.numerodepoliza = numerodepoliza
         self.fechadevencimiento = fechadevencimiento
+        self.equipamiento = equipamiento
+    def imprimir(self):
+        print(self.patente, self.seguro, self.numerodepoliza, self.fechadevencimiento, self.equipamiento)
+        print("")
 
 class consultas:
     def __init__(self, equipamientoderodado):
@@ -91,9 +101,7 @@ class registros_economicos:
         self.motivoingreso = motivoingreso
         self.motivoegreso = motivoegreso
         self.nfactura = nfactura
-#
-class Inicio(Screen):
-    pass
+
 KV = '''
 MDBoxLayout:
     orientation: "vertical"
@@ -106,8 +114,101 @@ MDBoxLayout:
         halign: "center"
 '''
 
-class asd(MDApp):
-    def build(self):
-        return Builder.load_string(KV)
 
-asd().run()#
+
+
+a = personal("Caleb", "Wharton", 95966365, "4-10-1979", "777", "+54 221 3101100", "cwharton@yahoo.com", "512, 954", "Aspirante" )
+b =personal("Aiden", "Walker",95966366,  "8-10-2000", "778 ","+54 221 3174023", "awalker@hotmail.com", "Los manantiales, 444", "Aspirante")
+c =personal("Jane", "Su", 95966367, "5-7-1980", "779" ," +54 221 3534666", "jsu@outlook.com", "Maracaibo, 678", "Cabo")
+d =personal("Jack", "Ross", 95966368, "9-9-1999", "780"," +54 221 4341806", "jackr@yahoo.com",  "hUTLINGHAM, 246", "Cadete" )
+e = personal("JJ", "Mayblank", 95966369, "2-5-1998", "781", "+54 221 4372972", "jjm@yahoo.com", "Caraval, 101", "Cadete" )
+f =personal("Sue", "MARTINEZ", 95966370, "8-8-1977","782","+54 221 4380831", "smartinez@yahoo.com", "Cerro Negro, 116", "Cabo" )
+g =personal("Scarlett", "Dragna", 95966371, "24-6-2001", "783","+54 221 5021283", "scarlettd@gmail.com",  "Jerusalen, 777", "Aspirante")
+h =personal("Carl", "White", 95966372, "5-2-2007", "784","+54 221 5716293", "cwhite@yahoo.com", "Chile, 234", "Aspirante")
+i =personal("Andrew", "Smith", 95966373, "15-9-1994", "785","+54 221 5856712", "asmith@gmail.com", "Washington DC, 453", "Jefe")
+j = personal("Aegan", "Cash", 95966374, "19-03-1997", "786" ,"+54 221 6613526", "yositehubieraelegidoati@hotmail.com","Tagus, 0319", "Aspirante")
+bomberoslista=[a,b,c,d,e,f,g,h,i,j]
+R1 = rodados("RPF-321","Rivadavia","12345678", "5-3-2025", "Vacío")
+R2 = rodados("PIF-430","Greenday","87654321", "18-1-2021", "Vacío" )
+R3 = rodados("JCJ-232","Sancor","45687312", "29-12-2028", "Vacío" )
+
+
+valor2=input("¿que quieres persona(1) o rodado(2)? ")
+if valor2 == "1":
+    print("""Ingrese los sisuientes datos:
+    Nombre
+    Apellido
+    DNI
+    Fecha de nacimiento
+    ID
+    Numero de telefono
+    Gmail
+    Domicilio
+    Rango""")
+    vari2= []
+    for persona in range (9):
+        jfdjfjk= input()
+        vari2.append(jfdjfjk)
+    ae=personal(vari2[0],vari2[1],vari2[2],vari2[3],vari2[4],vari2[5],vari2[6],vari2[7],vari2[8])
+else:
+    print("""Ingrese los siguientes datos:
+    Patente
+    Seguro
+    Numero de poliza
+    Fecha de vencimiento
+    Equipamiento""")
+    vari=[]
+    for rodado in range(5):
+        hdifej=input()
+        vari.append(hdifej)
+    aed=rodados(vari[0],vari[1],vari[2],vari[3],vari[4])
+
+a.imprimir()
+b.imprimir()
+c.imprimir()
+d.imprimir()
+e.imprimir()
+f.imprimir()
+g.imprimir()
+h.imprimir()
+i.imprimir()
+j.imprimir()
+try:
+    ae.imprimir()
+except:
+    pass
+R1.imprimir()
+R2.imprimir()
+R3.imprimir()
+try:
+    aed.imprimir()
+except:
+    pass
+hh=[]
+X=True
+while X==True:
+    print("Ingrese DNI de bomberos para dotación: ")
+    DNIdotacion = input()
+    try:
+        DNIdotacion=int(DNIdotacion)
+        hh.append(DNIdotacion)
+    except:
+        X=False
+        break
+
+#for b in bomberoslista:
+ #           for i in hh:
+        #        if b.dni == i:
+         #           break
+          #      else:
+           #         print("El bombero que llamó no está registrado")                                                      
+
+for q in bomberoslista:
+    if q.dni == DNIdotacion:
+        break
+    else:
+        print("El bombero que ingresó no existe")
+        break                               
+
+
+    
